3a8203f51e3aa1fb8c33eb8a809c85a152fd9fd0
Cer112 <46543271+Cer112@users.noreply.github.com>
2019-02-04 18:28:28 -0500

    Update shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,5 +1,9 @@
 # shopping_cart.py
 
+import datetime
+
+tax_rate = .06
+
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
     {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
@@ -38,7 +42,14 @@ while True:
         print("SELECTED_PRODUCT: " + matching_product["name"] + "" + str(matching_product["price"]
 
 
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    subtotal_price = subtotal_price + matching_product["price"]
+    print(" ... " + matching_product["name" + " (" + to_usd(matching_product["price"]) + ")")
 
+
+    
 
 
 
